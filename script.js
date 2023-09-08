9e33bfca4b9a956fe6ec5f7a1d5621c4b78a8d65
devhenode <devodehenry.2000@gmail.com>
2023-09-08 15:48:01 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function getCurrentDay() {
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+    const now = new Date();
+
+    const dayOfWeek = now.getDay();
+
+    return daysOfWeek[dayOfWeek]
+ 
+}
+
+function getCurrentTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+}
+
+const day = getCurrentDay()
+const currentTime = getCurrentTime();
+
+let date = document.getElementById("currentDayofTheWeek").innerHTML = "Day of the week: " + day;
+let moment = document.getElementById("currentUTCTime").innerHTML = "Current UTC time: " + currentTime;
+
+
